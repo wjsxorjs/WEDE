5f5b2c794146cf46f2a830b6b7fa35ddc7de825c
wjsxorjs <wjsxorjs@naver.com>
2024-04-30 09:56:21 +0900

    JAVA 2024.04.30 EXTRA2

--- Ex20240430/src/am/Main.java
+++ Ex20240430/src/am/Main.java
@@ -4,11 +4,15 @@ public class Main {
 
 	public static void main(String[] args) {
 		StaticTest st1 = new StaticTest(); // 모두들~ \n생성자
+		System.out.println();
 		System.out.println(StaticTest.msg); // 힘내라!
+		System.out.println();
 		System.out.println(StaticTest.getMsg()); // 모두들~ \n힘내라!
 
+		System.out.println();
 		StaticTest st2 = new StaticTest(); // 생성자
 		st2.msg = "대한민국";
+		System.out.println();
 		System.out.println(StaticTest.msg); // 대한민국
 		
 		
